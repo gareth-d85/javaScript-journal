a63b024a809d929d99cabd6d8959499aa5ad0e8b
Gareth Davies <gareth.d85@gmail.com>
2017-10-25 20:26:01 +0100

    coding anf learning
    writing out code as I remember things and researching

--- Varaiables/script.js
+++ Varaiables/script.js
@@ -1,1 +1,14 @@
 console.log("test"); //to test if I have linked to the html file.
+
+var firstName;
+var lastName;
+
+firstName = "Gareth";
+lastName = "Davies";
+
+console.log(firstName); //test to see if firstName value stored to it.
+
+alert(firstName + ' ' + lastName); // concatenation of firstName and lastName.
+
+document.write(firstName + ' ' + lastName); // writes to the webpage
+
