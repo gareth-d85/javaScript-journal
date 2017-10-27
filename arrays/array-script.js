046c30f7aae2fd05dde13442451eb0e01cdbf7b4
Gareth Davies <gareth.d85@gmail.com>
2017-10-27 15:17:56 +0100

    updated array
    I have updated the array to a more real life use

---
+++ arrays/array-script.js
@@ -0,0 +1,18 @@
+var employees = [
+	{
+		name: 'Gareth',
+		title: 'CNC operator',
+		wageBand: 'D',
+		startDate: '01/07/2016',
+		pension: true
+	},
+	{
+		name: 'Phil',
+		title: 'face & center',
+		wageBand: 'E',
+		startDate: '01/01/2017'
+		pension: false		
+	},
+
+
+];
