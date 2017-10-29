b271a67cd4ad6d5fc0fe1ffc146702b53b0486e4
Gareth Davies <gareth.d85@gmail.com>
2017-10-29 17:42:24 +0000

    added a scope folder and content

---
+++ scope/scope.js
@@ -0,0 +1,16 @@
+var globalScope = true;   // variables declared outside a function are global and can be
+var firstName = "Gareth";	// accessed inside a function (global scope)
+
+
+function local () {
+	localScope = true;  // localScope will only be accessable in the function
+	
+}
+
+
+console.log(globalScope); // I call the globalscope variable and it returns true.
+console.log(localScope); // I try to call the localScope outside the function and it returns not defined
+
+
+
+// I think this is how it works
