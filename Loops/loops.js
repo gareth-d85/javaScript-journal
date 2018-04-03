9456d11550633320702e02de4aa55eeea6c1bc1f
Gareth Davies <gareth.d85@gmail.com>
2018-04-03 19:41:45 +0100

    added a loops example folder and page

---
+++ Loops/loops.js
@@ -0,0 +1,24 @@
+// while loops
+
+var count = 0;
+
+console.log("Count from 0 to 10");
+while (count <= 10) {
+  document.write(count + " ")
+  count++
+} 
+/* All the time the while condition is true it will run, 
+but once it become false the loop stops */
+
+// Do while loops(need to add an example below)
+
+
+
+
+
+//for loops
+
+for (let i = 0; i <= 20; i+=2) {
+  console.log(i + " ")
+  
+}
